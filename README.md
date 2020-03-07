01e2cee4570b8b732949e0d023dd7b795d607351
Dexter <kyleres@live.com>
2020-03-07 10:20:16 -0600

    rehauled all code; updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 04-Homework - JavaScript Code Quiz
 
 ## Introduction
-For this homework assignment, we were tasked with creating JavaScript Code Quiz that has a timer and a penalty for wrong answers. This exercise was meant to develop our skills for Javascript and JQuery and to test our understanding of APIs.
+For this homework assignment, we were tasked with creating a JavaScript Code Quiz that has very specific rules. This exercise was meant to develop our Javascript skills, especially in terms of DOM manipulation and use of local storage.
 
 ## Acceptance Criteria
 ```
@@ -19,33 +19,40 @@ THEN I can save my initials and score
 ```
 
 ## Motivation
-My goal for this assignment was use my newly acquired JavaScript and JQuery knowledge to make a timed quiz where the HTML changes based on the script. I broke the assignment down to three parts:
+My goal for this assignment was to use my newly acquired JavaScript knowledge and push it further to make a timed quiz with the conditions metioned above. The challenge was to create this quiz using very little HTML and extensive JS scripts to drive the HTML changes. To begin, I broke the assignment down to three basic parts:
     1) Create and format a timer
-    2) Create the quiz (questions, answers, and scoring)
-    3) Create a scoreboard that stores can record previous scores
-The steps that went into creating each of these parts were challenging to complete. The most difficult part was trying to code the whole quiz in a way that correctly displays each question and its respective answers correctly. Another challenge for me was to make each of the questions appear in a different page and to score them appropriately before moving on to the next question. Overall, this was the hardest and most time-consuming activity so far. The knowledge of JS and JQuery reguired for this task was pretty comprehensive, and I spent a lot of time consulting outside sources.
+    2) Create the quiz
+    3) Create a scoreboard that stores and records previous scores
+The steps that went into creating each of these parts were challenging to complete. The most difficult part was trying to code the whole quiz in a way that correctly displays each question and its respective answers correctly. Another challenge for me was to make the scoreboard and use local storage to save high scores. Overall, this was the hardest and most time-consuming activity so far. The knowledge of JS required for this assignment was very comprehensive, and I had to spend a lot of time consulting outside sources.
 
 ## Version List
 ### v1.0
 * created README.md
-* revised index.html
-    * created timer div
-    * created quiz-container and content divs
-    * created buttons-container and button divs
-* created style.css
-    * added basic styling for better display appeal and responsiveness (nothing fancy)
-* added script.js
-    * created timer
-        * added timer variables
-        * formatted time to display HH:MM
-        * added function for countdown
-    * created quiz
-        * added quizQAs to hold questions and answers
-        * added quiz display variables
-            * added quiz display functionality
-        * added buttons variables
-            * added buttons functionality      
+* created index.html
+    * added timer
+    * added quiz div
+    * added scoreboard
+* created script.js
+    * added timer section
+        * added formatting function
+        * added countdown function
+    * added quiz section
+        * added quiz variables
+            * added questions and answers to questionList array
+        * added startQuiz function
+        * added renderQuestion function
+        * added checkAnswer function
+    * added scoreboard section
+        * added renderScore function
+            * added save button functionality
+            * set localStorage to save high scores
+                * added script to sort and splice scores to scoreboard   
 * tested application for functionality and responsiveness
+<!-- ### v2.0
+* added instructions
+* added more questions
+* created style.css
+    * added styling for better visual appeal and design -->
 
 ## Framework
 
@@ -54,6 +61,6 @@ Created with Bootstrap
 ## Credits
 * Sitepoint for some basic resources on making a simple JS quiz [here](https://www.sitepoint.com/simple-javascript-quiz/)
 * Code Explained for a decent breakdown of how to create a multiple choize quiz and render questions and answers. The blog post can be found [here](https://www.codeexplained.org/2018/10/create-multiple-choice-quiz-using-javascript.html), the Youtube video [here](https://youtu.be/49pYIMygIcU), and the GitHub repository [here](https://github.com/CodeExplainedRepo/Multiple-Choice-Quiz-JavaScript).
+* James Quick for help with the scoreboard and local storage scripts. His Youtube series on a similar project can be found [here](https://www.youtube.com/watch?v=u98ROZjBWy8&list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx), and his Github repository can be found [here](https://github.com/jamesqquick/Build-A-Quiz-App-With-HTML-CSS-and-JavaScript).
 * [w3schools.com](https://www.w3schools.com/) for all their resources regarding HTML, CSS and JS
-* [Eloquent Javascript](https://eloquentjavascript.net/) by Marijn Haverbeke for some in-depth reading about JS
 * Special thanks to my instructor Nick and my awesome TAs Jimi and Chris for answering all my questions and teaching me the skills I need to get this done!
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//Timer---------------------------------------------
+//Timer-----------------------------------------------------------
 //Timer Variables
-let totalTime = 0;
+let timer = document.getElementById("timer");
+let totalTime = 1.5 * 60 * 1000;
 let penaltyTime = 30 * 1000;
 let tickTime = 1000;
 let timeInterval = setInterval(tick, tickTime);
-let timer = document.getElementById('timer');
 
-//Clock Format Function
+//Timer Formatting Function
 function formatTime(ms) {
     let seconds = ms % 60000;
     let minutes = Math.floor(ms / 60000);
@@ -14,7 +14,7 @@ function formatTime(ms) {
     seconds = seconds /= 1000;
 
     if (seconds < 10) {
-        seconds = '0' + seconds
+        seconds = "0" + seconds;
     };
 
     timer.textContent = (`${ minutes }:${ seconds}`);
@@ -25,145 +25,203 @@ function tick() {
     formatTime(totalTime);
     totalTime -= tickTime;
 
-    if (totalTime <= 0) {
-        timer.textContent = ("00:00")
+    if (totalTime < 0) {
+        timer.textContent = "Time's up!";
+        renderScore();
     };
 };
 
-//Quiz----------------------------------------------
-//Quiz Questions and Answers
-let quizQAs = [
+//Quiz------------------------------------------------------------
+//Quiz HTML Variables
+let start = document.getElementById("start");
+let instructions = document.getElementById("instructions");
+let quizDiv = document.getElementById("quizDiv");
+let number = document.getElementById("number");
+let question = document.getElementById("question");
+let A = document.getElementById("A");
+let B = document.getElementById("B");
+let C = document.getElementById("C");
+let D = document.getElementById("D");
+
+//Questions and Answers List
+let questionList = [
     {
         question: "JavaScript must be inserted inside the following HTML tags:",
-        answers: {
-            a: "&lthead&gt...&lt/head&gt",
-            b: "&ltbody&gt...&lt/body&gt",
-            c: "&ltstyle&gt...&lt/style&gt",
-            d: "&ltscript&gt...&lt/script&gt",
-        },
-        correctAnswer: "d"
+        A: "A: &lthead&gt...&lt/head&gt",
+        B: "B: &ltbody&gt...&lt/body&gt",
+        C: "C: &ltstyle&gt...&lt/style&gt",
+        D: "D: &ltscript&gt...&lt/script&gt",
+        correctAnswer: "D",
     },
     {
-        question: "JavaScript must be written in the same HTML document so it can run successfully.",
-        answers: {
-            a: "True",
-            b: "False",
-        },
-        correctAnswer: "b"
+        question: "External JavaScript must be written in a document ending in ___ so it can run successfully.",
+        A: "A: .html",
+        B: "B: .js",
+        C: "C: .css",
+        D: "D: .cmd",
+        correctAnswer: "B",
     },
     {
-        question: "To use external script, the name of the file has to be inserted in the ___ attribute of the &ltscript&gt tag.",
-        answers: {
-            a: "src",
-            b: "href",
-            c: "class",
-            d: "id",
-        },
-        correctAnswer: "a"
+        question: "To use external script, the name of the file has to be inserted in the ___ attribute of the HTML's &ltscript&gt tag.",
+        A: "A: src",
+        B: "B: href",
+        C: "C: class",
+        D: "D: id",
+        correctAnswer: "A",
+    },
+    {
+        question: "___ are used for objects. ___ are used for arrays. ___ are used for function parameters.",
+        A: "A: {} , [] , ()",
+        B: "B: {} , () , []",
+        C: "C: () , {} , []",
+        D: "D: [] , {} , ()",
+        correctAnswer: "A",
+    },
+    {
+        question: "Sample code: </br><i>var sum = 3 + '4'</i></br> What does console.log(sum) display?",
+        A: "A: '4'",
+        B: "B: 7",
+        C: "C: 34",
+        D: "D: 12",
+        correctAnswer: "C",
+    },
+    {
+        question: ("Sample code: </br><i>var car = Honda</br>var Car = Mercedes</i></br> What does console.log(car) display?"),
+        A: "A: HondaMercedes",
+        B: "B: MercedesHonda",
+        C: "C: Honda",
+        D: "D: Mercedes",
+        correctAnswer: "C",
+    },
+    {
+        question: ("In Javascript, <i>var num = 3 - 1</i> will have the same value as:"),
+        A: "A: var num = 3; sum -= 1",
+        B: "B: var num = 2",
+        C: "C: var num = 3--",
+        D: "D: all of the above",
+        correctAnswer: "D",
+    },
+    {
+        question: ("In Javascript, the symbols + - * / and = are known as:"),
+        A: "A: operands",
+        B: "B: operators",
+        C: "C: operations",
+        D: "D: operas",
+        correctAnswer: "B",
+    },
+    {
+        question: ("Running the JS script <i>element.style.display = none</i> will mainly change the application's ___."),
+        A: "A: HTML",
+        B: "B: CSS",
+        C: "C: JS",
+        D: "D: JQuery",
+        correctAnswer: "B",
+    },
+    {
+        question: ("A true/false statement is also known as a:"),
+        A: "A: boolean",
+        B: "B: string literal",
+        C: "C: object",
+        D: "D: array",
+        correctAnswer: "A",
     },
 ];
 
-//Quiz Display Variables
-let quizDiv = document.getElementById("quiz");
-let scoreDiv = document.getElementById("score");
-let displayQAs = [];
-
-//Button Variables
-let startButton =document.getElementById("start");
-let submitButton = document.getElementById("submit");
-let previousButton = document.getElementById("previous");
-let nextButton = document.getElementById("next");
-let finishButton = document.getElementById("finish");
-
-//Scoring Variables
+//Quiz Tracking Variables
+let lastQuestion = questionList.length - 1;
+let currentQuestion = 0;
 let score = 0;
 
-//Questions & Answers Display
+//Start Quiz Function
 function startQuiz() {
-    quizQAs.forEach(
-        (currentQuestion, questionNumber) => {
-            let answers = [];
-            
-            for (letter in currentQuestion.answers) {
-                answers.push(
-                    `<label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}">
-                        ${letter} :
-                        ${currentQuestion.answers[letter]}
-                    </label>
-                    </br>`
-                );
-            }
-
-            displayQAs.push (
-                `<div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join("")} </div>
-                </br>`
-            );
-        }
-    );
-    
-    let i = 0;
-
-    quizDiv.innerHTML = displayQAs[i];
-    
-    function changeQuestion() {
-        i++;
-        quizDiv.innerHTML = displayQAs[i];
-    }
-
-    //Submit Question Function
-    function submitQuestion() {
-        let answerSelectors = quizDiv.querySelectorAll(".answers");
-
-        quizQAs.forEach(
-            (currentQuestion, questionNumber) => {
-                let answerSelector = answerSelectors[questionNumber];
-                let answerChecker = `input[name=question${questionNumber}]:checked`;
-                let chosenAnswer = (answerSelector.querySelector(answerChecker) || {}).value;
-
-                if (chosenAnswer == currentQuestion.correctAnswer) {
-                    score++;
-                    alert("Correct!");
-                    changeQuestion();
-                } else if (chosenAnswer !== currentQuestion.correctAnswer) {
-                    totalTime -= penaltyTime;
-                    alert("Wrong. The correct answer is " + `${currentQuestion.correctAnswer}`);
-                    changeQuestion();
-                };
-            }
-        );
-    };
-
-    //Next Button Functionality
-    nextButton.addEventListener("click", function() {
-        submitQuestion();
-    });
+    totalTime = 5 * 60 * 1000;
+    score = 0;
+    start.style.display = "none";
+    instructions.style.display = "none";
+    quizDiv.style.display = "block";
+    scoreboard.style.display = "none";
+    renderQuestion();
 };
 
-if (totalTime) {
-    nextButton.style.display = "none";
-} else {
-    nextButton.style.display = "inline";
-}
+//Render Question Function
+function renderQuestion() {
+    number.innerHTML= "Question " + (currentQuestion + 1);
+    let currentQ = questionList[currentQuestion];
 
-//Start Button Functionality
-startButton.addEventListener("click", function() {
-    startButton.style.display = "none";
-    timer.style.display = "inline";
-    totalTime = 5 * 60 * 1000;
-    startQuiz();
-});
+    question.innerHTML = currentQ.question;
+    A.innerHTML = currentQ.A;
+    B.innerHTML = currentQ.B;
+    C.innerHTML = currentQ.C;
+    D.innerHTML = currentQ.D;
+};
 
-//Finish Button Funcitonality
-finishButton.addEventListener("click", function() {
+//Check Answer Function
+function checkAnswer(answer) {
+    if (answer == questionList[currentQuestion].correctAnswer) {
+        alert("Correct!");
+        score++;
+    } else {
+        alert("Wrong! The correct answer is " + questionList[currentQuestion].correctAnswer);
+        totalTime -= 30 * 1000;
+    };
+
+    if (currentQuestion < lastQuestion) {
+        currentQuestion++;
+        renderQuestion();
+    } else {
+        renderScore();
+    };
+};
+
+start.addEventListener("click", startQuiz);
+ 
+//Scoreboard------------------------------------------------------
+//Scoreboard Variables
+let scoreboard = document.getElementById("scoreboard");
+let saveScore = document.getElementById("saveScore");
+let finalScore = document.getElementById("finalScore");
+let highScoresTable = document.getElementById("highScoresTable");
+let finalScoreValue = localStorage.getItem("finalScoreValue");
+let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+let maxHighScores = 5;
+
+//Render Score Function
+function renderScore() {
     totalTime = 0;
+    tickTime = 0;
+    finalScoreValue = score;
+    localStorage.setItem("finalScoreValue", score);
+    
+    instructions.style.display = "none";
+    start.style.display = "none";
     quizDiv.style.display = "none";
-    nextButton.style.display = "none";
-    finishButton.style.display = "none";
-    startButton.style.display = "initial";
-    scoreDiv.style.display = "inline";
-    scoreDiv.innerHTML = `${score} out of ${quizQAs.length}`;
-});
+    scoreboard.style.display = "block";
 
+    finalScore.innerText = "Your final score is " + finalScoreValue + " out of 10. Refresh the page to try again.";
 
+    //Save Button Functionality
+    saveScore.addEventListener("click", function() {
+        // object to store user's name and score
+        let score = {
+            name: username.value,
+            score: finalScoreValue,
+        };
+        // push object to highscore array
+        highScores.push(score);
+        // sort scores from highest to lowest
+        highScores.sort((a, b) => b.score - a.score);
+        // splice score at last position
+        highScores.splice(5);
+        // save highscores to local storage as string
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+    });
+    // for displaying highscores in scoreboard 
+    highScoresTable.innerHTML += highScores
+        .map(score => {
+            return `
+                <tr>
+                    <td>${score.name}</td>
+                    <td>${score.score}</td>
+                </tr>`
+        });
+};
